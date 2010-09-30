02ea69f5c67818b5c0e2e82223918f999d6a5b0c
Lluis Gili <lluis@ingent.net>
2010-09-30 15:31:33 +0200

    download and install mcollective rpm and deb packages

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -2,12 +2,17 @@ class mcollective::install::debian {
 
     include stomp
 
-    package { "mcollective":
-        ensure => $mcollective_package_version ? {
-            "" => present,
-            default => $mcollective_package_version
-        },
-        require => Package["stomp"],
+    exec {
+      "download mcollective":
+          command => "cd /usr/local/src ; wget http://mcollective.googlecode.com/files/mcollective_0.4.9-1_all.deb",
+          creates => "/usr/local/src/mcollective_0.4.9-1_all.deb";
+      "download mcollective-common":
+          command => "cd /usr/local/src ; wget http://mcollective.googlecode.com/files/mcollective-common_0.4.9-1_all.deb",
+          creates => "/usr/local/src/mcollective-common_0.4.9-1_all.deb";
+      "install mcollective":
+          command => "cd /usr/local/src ; dpkg -i mcollective_0.4.9-1_all.deb mcollective-common_0.4.9-1_all.deb",
+          unless => "dpkg -L mcollective &> /dev/null",
+          require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ];
     }
 
 }
--- manifests/install/redhat.pp
+++ manifests/install/redhat.pp
@@ -3,12 +3,17 @@ class mcollective::install::redhat {
 
     include stomp
 
-    package { "mcollective":
-        ensure => $mcollective_package_version ? {
-            "" => present,
-            default => $mcollective_package_version
-        },
-        require => Package["stomp"],
+    exec {
+      "download mcollective":
+          command => "cd /usr/local/src ; wget http://mcollective.googlecode.com/files/mcollective-0.4.9-1.el5.noarch.rpm",
+          creates => "/usr/local/src/mcollective-0.4.9-1.el5.noarch.rpm";
+      "download mcollective-common":
+          command => "cd /usr/local/src ; wget http://mcollective.googlecode.com/files/mcollective-common-0.4.9-1.el5.noarch.rpm",
+          creates => "/usr/local/src/mcollective-common-0.4.9-1.el5.noarch.rpm";
+      "install mcollective":
+          command => "cd /usr/local/src ; rpm -i mcollective-0.4.9-1.el5.noarch.rpm mcollective-common-0.4.9-1.el5.noarch.rpm",
+          unless => "dpkg -ql mcollective &> /dev/null",
+          require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ];
     }
 
 }
