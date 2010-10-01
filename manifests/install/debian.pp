b3687dc1f616e1bd7b0be6366b8780280ad19c32
Lluis Gili <lluis@ingent.net>
2010-10-01 14:54:34 +0200

    use cwd on exec

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -4,14 +4,17 @@ class mcollective::install::debian {
 
     exec {
       "download mcollective":
-          command => "wget http://mcollective.googlecode.com/files/mcollective_0.4.9-1_all.deb -P /usr/local/src",
+          command => "wget http://mcollective.googlecode.com/files/mcollective_0.4.9-1_all.deb",
+          cwd => "/usr/local/src",
           creates => "/usr/local/src/mcollective_0.4.9-1_all.deb";
       "download mcollective-common":
-          command => "wget http://mcollective.googlecode.com/files/mcollective-common_0.4.9-1_all.deb -P /usr/local/src",
+          command => "wget http://mcollective.googlecode.com/files/mcollective-common_0.4.9-1_all.deb",
+          cwd => "/usr/local/src",
           creates => "/usr/local/src/mcollective-common_0.4.9-1_all.deb";
       "install mcollective":
-          command => "dpkg --force-confold -i /usr/local/src/mcollective_0.4.9-1_all.deb /usr/local/src/mcollective-common_0.4.9-1_all.deb",
+          command => "dpkg --force-confold -i mcollective_0.4.9-1_all.deb mcollective-common_0.4.9-1_all.deb",
           unless => "dpkg -L mcollective &> /dev/null",
+          cwd => "/usr/local/src",
           require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ];
     }
 
--- manifests/install/redhat.pp
+++ manifests/install/redhat.pp
@@ -5,13 +5,16 @@ class mcollective::install::redhat {
 
     exec {
       "download mcollective":
-          command => "wget http://mcollective.googlecode.com/files/mcollective-0.4.9-1.el5.noarch.rpm -P /usr/local/src",
+          command => "wget http://mcollective.googlecode.com/files/mcollective-0.4.9-1.el5.noarch.rpm",
+          cwd => "/usr/local/src",
           creates => "/usr/local/src/mcollective-0.4.9-1.el5.noarch.rpm";
       "download mcollective-common":
-          command => "wget http://mcollective.googlecode.com/files/mcollective-common-0.4.9-1.el5.noarch.rpm -P /usr/local/src",
+          command => "wget http://mcollective.googlecode.com/files/mcollective-common-0.4.9-1.el5.noarch.rpm",
+          cwd => "/usr/local/src",
           creates => "/usr/local/src/mcollective-common-0.4.9-1.el5.noarch.rpm";
       "install mcollective":
-          command => "rpm -i /usr/local/src/mcollective-0.4.9-1.el5.noarch.rpm /usr/local/src/mcollective-common-0.4.9-1.el5.noarch.rpm --nodeps",
+          command => "rpm -i mcollective-0.4.9-1.el5.noarch.rpm mcollective-common-0.4.9-1.el5.noarch.rpm --nodeps",
+          cwd => "/usr/local/src",
           unless => "rpm -ql mcollective &> /dev/null",
           require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ];
     }
