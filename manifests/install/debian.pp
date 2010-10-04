ac8f47857311fa28d1ccc6902dd7a20b134b6837
Lluis Gili <lluis@ingent.net>
2010-10-04 15:04:16 +0200

    ignore rubygems dependency

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -12,7 +12,7 @@ class mcollective::install::debian {
           cwd => "/usr/local/src",
           creates => "/usr/local/src/mcollective-common_0.4.9-1_all.deb";
       "install mcollective":
-          command => "dpkg --force-confold -i mcollective_0.4.9-1_all.deb mcollective-common_0.4.9-1_all.deb",
+          command => "dpkg --ignore-depends=rubygems --force-confold -i mcollective_0.4.9-1_all.deb mcollective-common_0.4.9-1_all.deb",
           unless => "dpkg -L mcollective &> /dev/null",
           cwd => "/usr/local/src",
           require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ];
