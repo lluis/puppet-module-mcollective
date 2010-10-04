690b668497f0fddf0909da3945cad7bf11730fa9
Lluis Gili <lluis@ingent.net>
2010-10-04 15:31:10 +0200

    use install::redhat on Fedora

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@
 class mcollective::install {
     case $operatingsystem {
         ubuntu,debian: { include mcollective::install::debian }
-        redhat,centos: { include mcollective::install::redhat }
+        redhat,centos,fedora: { include mcollective::install::redhat }
         default: { fail("${hostname}: mcollective: trying to install unsupported operatingsystem $operatingsystem") }
     }
 }
