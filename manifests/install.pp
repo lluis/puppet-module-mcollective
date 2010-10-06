53b3dea1ec80cfd577d7f6345d39e5d964a38892
Lluis Gili <lluis@ingent.net>
2010-10-06 14:36:02 +0200

    include rubygems on install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,9 @@
-
 class mcollective::install {
-    case $operatingsystem {
-        ubuntu,debian: { include mcollective::install::debian }
-        redhat,centos,fedora: { include mcollective::install::redhat }
-        default: { fail("${hostname}: mcollective: trying to install unsupported operatingsystem $operatingsystem") }
-    }
+  include rubygems
+  case $operatingsystem {
+    ubuntu,debian: { include mcollective::install::debian }
+    redhat,centos,fedora: { include mcollective::install::redhat }
+    default: { fail("${hostname}: mcollective: trying to install unsupported operatingsystem $operatingsystem") }
+  }
 }
 
