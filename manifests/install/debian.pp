2c23fb87e4e6bb05dd2d6c93e9eac7a4334c9c67
Lluis Gili <lluis@ingent.net>
2010-10-20 15:45:39 +0200

    update mcollective from 0.4.9 to 0.4.10

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -1,19 +1,24 @@
 class mcollective::install::debian {
 
+    $ver="0.4.10-1"
+    $mcollective="mcollective_${ver}_all.deb"
+    $mcollective_common="mcollective-common_${ver}_all.deb"
+
     exec {
       "download mcollective":
-          command => "wget http://mcollective.googlecode.com/files/mcollective_0.4.9-1_all.deb",
+          command => "wget http://mcollective.googlecode.com/files/$mcollective",
           cwd => "/usr/local/src",
-          creates => "/usr/local/src/mcollective_0.4.9-1_all.deb";
+          creates => "/usr/local/src/$mcollective";
       "download mcollective-common":
-          command => "wget http://mcollective.googlecode.com/files/mcollective-common_0.4.9-1_all.deb",
+          command => "wget http://mcollective.googlecode.com/files/$mcollective_common",
           cwd => "/usr/local/src",
-          creates => "/usr/local/src/mcollective-common_0.4.9-1_all.deb";
+          creates => "/usr/local/src/$mcollective_common";
       "install mcollective":
-          command => "dpkg --ignore-depends=rubygems --force-confold -i mcollective_0.4.9-1_all.deb mcollective-common_0.4.9-1_all.deb",
-          unless => "dpkg -l | grep mcollective | grep '^ii' &> /dev/null",
+          command => "dpkg --ignore-depends=rubygems --force-confold -i $mcollective $mcollective_common",
+          unless => "dpkg -l | grep 'mcollective ' | grep '^ii' | grep '$ver' &> /dev/null",
           cwd => "/usr/local/src",
-          require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ];
+          require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ],
+          notify => Service["mcollective"];
     }
 
 }
--- manifests/install/redhat.pp
+++ manifests/install/redhat.pp
@@ -1,20 +1,25 @@
 
 class mcollective::install::redhat {
 
+    $ver="0.4.10-1.el5"
+    $mcollective="mcollective-$ver.noarch.rpm"
+    $mcollective_common="mcollective-common-$ver.noarch.rpm"
+
     exec {
       "download mcollective":
-          command => "wget http://mcollective.googlecode.com/files/mcollective-0.4.9-1.el5.noarch.rpm",
+          command => "wget http://mcollective.googlecode.com/files/$mcollective",
           cwd => "/usr/local/src",
-          creates => "/usr/local/src/mcollective-0.4.9-1.el5.noarch.rpm";
+          creates => "/usr/local/src/$mcollective";
       "download mcollective-common":
-          command => "wget http://mcollective.googlecode.com/files/mcollective-common-0.4.9-1.el5.noarch.rpm",
+          command => "wget http://mcollective.googlecode.com/files/$mcollective_common",
           cwd => "/usr/local/src",
-          creates => "/usr/local/src/mcollective-common-0.4.9-1.el5.noarch.rpm";
+          creates => "/usr/local/src/$mcollective_common";
       "install mcollective":
-          command => "rpm -i mcollective-0.4.9-1.el5.noarch.rpm mcollective-common-0.4.9-1.el5.noarch.rpm --nodeps",
+          command => "rpm -U $mcollective $mcollective_common --nodeps",
           cwd => "/usr/local/src",
-          unless => "rpm -ql mcollective &> /dev/null",
-          require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ];
+          unless => "rpm -ql mcollective-$ver &> /dev/null",
+          require => [ Exec["download mcollective"] , Exec["download mcollective-common"] ],
+          notify => Service["mcollective"];
     }
 
 }
